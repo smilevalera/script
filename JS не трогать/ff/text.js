061f96c38f4b78af72add0038be898f3393fbbaa
smilevalera <91686121+smilevalera@users.noreply.github.com>
2023-01-24 18:52:54 +0300

    Add files via upload

---
+++ JS не трогать/ff/text.js
@@ -0,0 +1,3 @@
+function doSomeone(){
+    console.log(123);
+}
---
+++ JS не трогать/index.js
@@ -0,0 +1,27 @@
+
+
+
+    let container = document.querySelector('.container');
+
+    
+    
+    let arr = [];
+    for(let i = 0; i <arr.length; i++){
+        let value = document.createElement('div');
+        value.classList.add('fit');
+        container.append(value);
+        let value1 = document.createElement('div');
+        value1.classList.add('block1');
+        container.append(value);
+        let value2 = document.createElement('div');
+        value2.classList.add('block2');
+        container.append(value);
+        let value3 = document.createElement('div');
+        value3.classList.add('');
+        container.append(value);
+
+    }
+
+
+
+
